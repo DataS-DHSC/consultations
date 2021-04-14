cedafab43f341d766f5bc92979a034f11b9df543
MHWauben <martine.wauben@gmail.com>
2021-04-14 13:48:48 +0100

    Barplot function adapted to output from new response_tables function

--- R/survey_response_barplot.R
+++ R/survey_response_barplot.R
@@ -1,29 +1,33 @@
 #' Visualise responses
 #'
-#' Take a consultation response spreadsheet (as transformed by Hmisc::describe), and turn it into a barplot.
+#' Take a consultation response spreadsheet, and turn it into a barplot for each column.
+#' This uses the question_types() function to clean data first, including aggregating small categories and detecting free text columns.
+#' It then makes barplots on the basis of the frequency tables produced by response_tables().
 #'
-#' @param survey_response list
+#' @param dummy_response dataframe
 #'
-#' @return A ggplot2 object
+#' @return list of ggplot2 objects
 #' @export
 #'
-#' @examples survey_response_barplot(Hmisc::describe(dummy_response))
-survey_response_barplot <- purrr::safely(function(survey_response){
-  survey_barplot <- survey_response[[1]]$result %>%
-    ggplot2::ggplot(ggplot2::aes(x = Response, y = Frequency, fill = Response))+
-    ggplot2::geom_col()+
-    ggplot2::theme(axis.title.x = ggplot2::element_blank())+
-    ggplot2::scale_fill_viridis_d()+
-    ggplot2::labs(x = "", y = "Number of responses")+
-    ggthemes::theme_calc()+
-    ggplot2::theme(
-      legend.position = "none",
-      plot.background = ggplot2::element_blank(),
-      legend.direction = "horizontal",
-      legend.title = ggplot2::element_blank(),
-      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)
-    )+
-    ggplot2::ggtitle(names(survey_response))
-
-  return(survey_barplot)
-})
+#' @examples survey_response_barplot(dummy_response)
+survey_response_barplot <- function(dummy_response){
+  response_t <- response_tables(dummy_response, question_types(dummy_response))
+  plots <- list()
+  for (i in names(response_t)){
+    plots[[i]] <- response_t[[i]] %>%
+      ggplot2::ggplot(ggplot2::aes(x = Response, y = Frequency, fill = Response))+
+      ggplot2::geom_col()+
+      ggplot2::theme(axis.title.x = ggplot2::element_blank())+
+      ggplot2::scale_fill_viridis_d()+
+      ggplot2::labs(x = "", y = "Number of responses")+
+      ggthemes::theme_calc()+
+      ggplot2::theme(
+        legend.position = "none",
+        plot.background = ggplot2::element_blank(),
+        legend.direction = "horizontal",
+        legend.title = ggplot2::element_blank(),
+        axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))+
+      ggplot2::ggtitle(i)
+  }
+  return(plots)
+}
