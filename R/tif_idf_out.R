e6825ee00d47598631bc10e860cfac0eff193eee
MHWauben <martine.wauben@gmail.com>
2021-04-15 11:15:59 +0100

    tf-idf base functions, not yet including customisation

---
+++ R/tif_idf_out.R
@@ -0,0 +1,31 @@
+#' Calculate TF-IDF scores
+#'
+#' @param data pre-cleaned response data
+#' @param grouping_var column that groups each row. May be with (string) or without (tidy evaluation) quotation marks
+#'
+#' @return dataframe with a row for each group-word combination, and the tf-idf score
+#' @export
+#'
+#' @examples tf_idf_out(unnest_stem_and_remove_stopwords(dummy_response, colnames(dummy_response)[7], "words"), colnames(dummy_response)[1])
+tf_idf_out <- function(data, grouping_var){
+  # Allow for both string input and tidyeval using quosures for the grouping_var
+  if(is.character(grouping_var)){
+    grouping_var <- sym(grouping_var)
+  } else {
+    grouping_var <- rlang::enquo(grouping_var)
+  }
+
+
+  tf_idf_out <- data %>%
+    dplyr::count(!!grouping_var, word, sort = TRUE) %>%
+    dplyr::group_by(!!grouping_var) %>%
+    dplyr::mutate(total = sum(n)) %>%
+    dplyr::arrange(-n) %>%
+    dplyr::mutate(rank = row_number(),
+           `term frequency` = n/total) %>%
+    tidytext::bind_tf_idf(word, !!grouping_var, n) %>%
+    dplyr::arrange(desc(tf_idf)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(word = factor(word, levels = rev(unique(word))))
+  return(tf_idf_out)
+}
---
+++ R/unnest_stem_and_remove_stopwords.R
@@ -0,0 +1,19 @@
+#' Pre-clean consultation responses
+#'
+#' @param data dataframe of responses
+#' @param text_col column name containing free text to be analysed
+#' @param token what text entity to be analysed? by default word-by-word, but other options are defined in tidytext::unnest_tokens
+#'
+#' @return dataframe with stemmed, unnested words with removed stopwords
+#' @export
+#'
+#' @examples unnest_stem_and_remove_stopwords(dummy_response, colnames(dummy_response)[7], "words")
+unnest_stem_and_remove_stopwords <- function(data, text_col, token = "words"){
+  dummy_response %>%
+    tidytext::unnest_tokens(output = word, input = text_col, token = token) %>%
+    dplyr::anti_join(tidytext::get_stopwords(), by = "word") %>% # Remove stop-words
+    dplyr::filter(!grepl("^[0-9]{1,}$", word)) %>% # Get rid of number-only 'words'
+    dplyr::filter(!grepl("^[vxi]+$", word)) %>% # Get rid of roman numerals
+    dplyr::mutate(word = hunspell::hunspell_stem(word)) %>% # Stem words to group better
+    tidyr::unnest(word)
+}
