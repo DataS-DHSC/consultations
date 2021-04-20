ef921cb37eacc478e5516a56153f59e489bd16ab
MHWauben <martine.wauben@gmail.com>
2021-04-20 12:10:01 +0100

    Separate out removal and stemming functions so they can be applied separately

---
+++ R/text_remove_words.R
@@ -0,0 +1,42 @@
+#' Remove words before analysis
+#'
+#' After unnesting the words from the full free-text column, you may want to filter out certain groups of words:
+#' stopwords like 'and' or 'the', profanity, number-only words, or a list of custom words.
+#' This function lets you do this easily.
+#'
+#' @param unnest_data dataframe with unnested free text data (a row per word, eg. as prepared by tidytext::unnest_tokens)
+#' @param word_col column name containing word tokens
+#' @param stopwords do you want to remove stopwords? TRUE/FALSE
+#' @param profanity do you want to remove profanity? TRUE/FALSE
+#' Default profanity list here: https://www.cs.cmu.edu/~biglou/resources/bad-words.txt
+#' @param number_only do you want to remove "words" that are only numbers?
+#' These are usually years or phone numbers. TRUE/FALSE
+#' @param custom_words do you have a custom list of words to remove? Must be entered as a string vector.
+#'
+#' @return dataframe with rows filtered out
+#' @export
+#'
+#' @examples text_remove_words(data.frame(doc_id = c(1, 2, 3, 4), word = c('1', 'test', 'the', 'function')), word_col = 'word')
+text_remove_words <- function(unnest_data, word_col = "word", stopwords = TRUE, profanity = TRUE, number_only = TRUE, custom_words = c("")){
+  data_out <- unnest_data
+  # Remove stopwords?
+  if(stopwords){
+    data_out <- dplyr::anti_join(data_out, tidytext::get_stopwords(), by = word_col)
+  }
+  # Remove profanity?
+  if(profanity){
+    prof <- read.table('https://www.cs.cmu.edu/~biglou/resources/bad-words.txt', stringsAsFactors = FALSE)
+    colnames(prof) <- word_col
+    data_out <- dplyr::anti_join(data_out, prof, by = word_col)
+  }
+  # Remove number-only words?
+  if(number_only){
+    data_out <- data_out[!(grepl('^[0-9]{1,}$', data_out[word_col]))]
+  }
+  # Remove custom word list?
+  if(length(custom_words) > 0){
+    data_out <- data_out[!(data_out[word_col] %in% custom_words)]
+  }
+
+  return(data_out)
+}
--- R/text_unnest_stem_and_remove_stopwords.R
+++ R/text_unnest_stem_and_remove_stopwords.R
@@ -1,19 +1,21 @@
 #' Pre-clean consultation responses
 #'
+#' Takes a dataframe with a free-text column as input. Unnests words from the free text, removes stopwords, profanity, and number-only words,
+#' removes an optional custom list of words, and stems words (using hunspell) so they can be grouped better.
+#'
+#' Note that, although you may choose a token other than "words" (like "sentences"), the word removal and stemming expects word tokens only.
+#'
 #' @param data dataframe of responses
 #' @param text_col name of column containing free text to be analysed
 #' @param token what text entity to be analysed? by default word-by-word, but other options are defined in tidytext::unnest_tokens
 #'
-#' @return dataframe with stemmed, unnested words with removed stopwords
+#' @return dataframe with stemmed, unnested words with removed words
 #' @export
 #'
 #' @examples text_unnest_stem_and_remove_stopwords(dummy_response, colnames(dummy_response)[7], "words")
-text_unnest_stem_and_remove_stopwords <- function(data, text_col, token = "words"){
-  dummy_response %>%
+text_unnest_remove_stem_words <- function(data, text_col, token = "words", custom_words = c("")){
+  data %>%
     tidytext::unnest_tokens(output = word, input = text_col, token = token) %>%
-    dplyr::anti_join(tidytext::get_stopwords(), by = "word") %>% # Remove stop-words
-    dplyr::filter(!grepl("^[0-9]{1,}$", word)) %>% # Get rid of number-only 'words'
-    dplyr::filter(!grepl("^[vxi]+$", word)) %>% # Get rid of roman numerals
-    dplyr::mutate(word = hunspell::hunspell_stem(word)) %>% # Stem words to group better
-    tidyr::unnest(word)
+    text_remove_words(., custom_words = custom_words) %>%
+    text_stem_words(.)
 }
